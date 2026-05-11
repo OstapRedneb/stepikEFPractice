3cd69a9e0d56bac500f43c2222faebd2366940f0
OstapRedneb <rodionzabirov888@gmail.com>
2026-05-11 22:18:34 +0500

    Снова поиграл с миграциями

--- stepikEFPractice/Program.cs
+++ stepikEFPractice/Program.cs
@@ -8,9 +8,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        using ApplicationDbContext context = new ApplicationDbContext();
+        //using ApplicationDbContext context = new ApplicationDbContext();
 
-        Console.WriteLine(string.Join('\n', context.Users.Select(user => user.Full_Name)));
-        Console.WriteLine("Sucsess");
+        //Console.WriteLine(string.Join('\n', context.Users.Select(user => user.Full_Name)));
+        //Console.WriteLine("Sucsess");
     }
 }
